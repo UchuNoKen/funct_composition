ce16c6aa501c4cfa99f77a182085e0cf0f28bd80
UchuNoKen <xen.o.fite@gmail.com>
2019-03-25 18:28:06 -0400

    initial commit

---
+++ intro/003reduce.js
@@ -0,0 +1,6 @@
+// reduce()
+// aka: fold, accumulate
+
+// iterate over a list
+// applying a function to an accumulated value and the next item in the list
+// until iteration is complete, and the accumulated value is returned
