4f596d20c1c384a743fbcf1b4511d7678ee75c69
UchuNoKen <xen.o.fite@gmail.com>
2019-03-26 09:48:46 -0400

    abstraction lesson

---
+++ intro/005abstractionComposition.js
@@ -0,0 +1,44 @@
+// Abstraction -----------------------------------------------------------
+
+// abstraction: the process of considering something independently of its associations,
+// attributes, or concrete accompaniments
+
+// !! software solutions should be decomposable into their component parts, and
+// recomposable into new solutions, without changing the internal component
+// implementation details
+
+// Abstraction is the act of simplification -----------------------------
+
+// The process of abstraction has two main components:
+
+// Generalization: the process of finding similarities (the obvious) in repeated patterns, and
+// and hiding similarities behind an abstraction
+
+// Specialization: the process of using the abstraction, supplying only what is different
+// (the meaningful) for each use case
+
+// Abstraction is the process of extracting the underlying essence of a concept.
+
+// By exploring common ground between different problems from different domains, we learn how to
+// step outside our headspace for a moment and see a problem from a different perspective.
+
+// Abstraction in software --------------------------------------
+
+// abstraction in software takes many forms:
+//  - algorithms
+//  - data structures
+//  - modules
+//  - classes
+//  - frameworks
+//  - and functions
+
+// Functions make great abstractions because they posses the qualities that are
+// essential for good abstraction:
+
+// Identity - ability to assign a name to it and reuse it in different contexts
+
+// Composition - The ability to compose simple functions to form more complex functions
+
+// Abstraction through composition --------------------------------------
+
+// pure functions are the most useful for abstraction
